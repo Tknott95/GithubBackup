7f803718eb80526a427056b2f4a4aaea04d39892
tknott95 <tknott95@hotmail.com>
2024-10-26 09:20:35 -0600

    WORKING PRIVATE AND PUBLIC

--- backup.sh
+++ backup.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 # MUST SETUP gh auth as api style via curl only hits public repos (github-cli)
-# ./backup.sh <BACKUP-LOCATION>
-rm -f repos.txt ; touch repos.txt
-
 # the only way to get private repos also is to use gh to list them
 # Setup gh auth
+
+EXAMPLE
+# ./backup.sh <BACKUP-LOCATION>
+# ./backup.sh ~/Documents
+
+rm -f repos.txt ; touch repos.txt
+
 
 # PUBLIC AND PRIVATE - HTTPS or SSH
 # gh repo list --json url --limit 9999 | jq .[].url >> repos.txt
 gh repo list --json sshUrl --limit 9999 | jq .[].sshUrl >> repos.txt
 
 mapfile -t repos_array < <(sed 's/^"\(.*\)"$/\1/;s/^[ \t]*//;s/[ \t]*$//' repos.txt)
-# sed uses regex to remove " from repos.txt as it was breaking git clone
+# sed uses regex to remove " from repos.txt lines as it was breaking the git clone command
 
 for temp_repo_name in "${repos_array[@]}"; do
   echo -e "\n  CLONING: $temp_repo_name"
