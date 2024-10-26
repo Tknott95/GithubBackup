db71e8a10f938b2bf9bef6d8ebac868a1cf3000b
tknott95 <tknott95@hotmail.com>
2024-10-26 08:57:37 -0600

    Using gh auth for private and public

--- backup.sh
+++ backup.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-# ./backup.sh <username>
+# PAGE=1
+
+# source .env
+
+
+
+# Iif wanting to optmize from length (works on public only)
+# curl -H "Authorization: token $GITHUB_API_KEY" -s "https://api.github.com/users/$1" | jq '.public_repos'
+
+# Only saves public - assumes less than 900
+# for PAGE in {1..9}; do
+#   curl -H "Authorization: token $GITHUB_API_KEY" -s "https://api.github.com/users/$1/repos?per_page=1000&page=$PAGE" | jq -r .[].clone_url >> repos.txt
+# done
+
+
+# MUST SETUP gh auth  (github-cli)
+# ./backup.sh
 rm -f repos.txt ; touch repos.txt
-curl "https://api.github.com/users/$1/repos?per_page=1000" | jq -r .[].clone_url >> repos.txt
+
+# the only way to get private repos also is to use gh to list them
+# Setup gh auth
+
+# PUBLIC AND PRIVATE
+gh repo list --json url --limit 9999 | jq .[].url >> repos.txt
