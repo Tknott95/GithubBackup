0cd86de05299c263979beb380b4c3ebca05d25df
tknott95 <tknott95@hotmail.com>
2024-10-26 09:19:31 -0600

    WORKING PRIVATE AND PUBLIC

--- backup.sh
+++ backup.sh
@@ -1,36 +1,20 @@
 #!/bin/sh
 
-# PAGE=1
-
-# source .env
-
-
-
-# Iif wanting to optmize from length (works on public only)
-# curl -H "Authorization: token $GITHUB_API_KEY" -s "https://api.github.com/users/$1" | jq '.public_repos'
-
-# Only saves public - assumes less than 900
-# for PAGE in {1..9}; do
-#   curl -H "Authorization: token $GITHUB_API_KEY" -s "https://api.github.com/users/$1/repos?per_page=1000&page=$PAGE" | jq -r .[].clone_url >> repos.txt
-# done
-
-
-# MUST SETUP gh auth  (github-cli)
+# MUST SETUP gh auth as api style via curl only hits public repos (github-cli)
 # ./backup.sh <BACKUP-LOCATION>
 rm -f repos.txt ; touch repos.txt
 
 # the only way to get private repos also is to use gh to list them
 # Setup gh auth
 
-# PUBLIC AND PRIVATE
+# PUBLIC AND PRIVATE - HTTPS or SSH
 # gh repo list --json url --limit 9999 | jq .[].url >> repos.txt
 gh repo list --json sshUrl --limit 9999 | jq .[].sshUrl >> repos.txt
 
 mapfile -t repos_array < <(sed 's/^"\(.*\)"$/\1/;s/^[ \t]*//;s/[ \t]*$//' repos.txt)
-# mapfile -t repos_array < repos.txt
+# sed uses regex to remove " from repos.txt as it was breaking git clone
 
 for temp_repo_name in "${repos_array[@]}"; do
-  # cd $1
   echo -e "\n  CLONING: $temp_repo_name"
-  git clone "$temp_repo_name" $1
+  git clone $temp_repo_name $1
 done
