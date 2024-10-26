4f04549167d6c55bf1a81653c11b1b27d1fed959
tknott95 <tknott95@hotmail.com>
2024-10-26 08:21:11 -0600

    Github Backups. Fetching all names from curl

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # ./backup.sh <username>
-rm -f repos.txt
-curl "https://api.github.com/users/$1/repos?per_page=1000" | jq -r .[].clone_url | repos.txt
+rm -f repos.txt ; touch repos.txt
+curl "https://api.github.com/users/$1/repos?per_page=1000" | jq -r .[].clone_url >> repos.txt
